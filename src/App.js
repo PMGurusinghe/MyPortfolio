a9c8a6dfa3baf0049ec248b68ebd52f8fec236f3
PMGurusinghe <madhushangurusinghe@gmail.com>
2023-09-28 22:50:17 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Portfolio from "./components/Portfolio";
+// import Portfolio from "./components/Portfolio";
 import Services from "./components/Services";
-import Testimonials from "./components/Testimonials";
+// import Testimonials from "./components/Testimonials";
 
 function App() {
 
